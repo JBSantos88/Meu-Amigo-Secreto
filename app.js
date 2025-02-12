2703acd25ca1cdcddc7b52a8ac4a64a78e4bb6c0
Belarmino Santos <belarminosn@gmail.com>
2025-02-12 17:33:13 -0300

    adicionando funcionalidades dadd e sortear

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const nomesAmigos = [];
 
 function atualizarLista() {
     const lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
+    lista.innerHTML = ""; 
 
     for (let i = 0; i < nomesAmigos.length; i++) {
         const li = document.createElement("li");
@@ -15,9 +15,26 @@ function adicionarAmigo() {
     const input = document.getElementById("amigo");
     const nome = input.value.trim();
     
-    if (nome !== "" && !nomesAmigos.includes(nome)) {
+    if (nome === "") {
+        alert("Insira um nome!");
+        return;
+    }
+    
+    if (!nomesAmigos.includes(nome)) {
         nomesAmigos.push(nome);
         atualizarLista();
-        input.value = "";
     }
+    input.value = ""; 
+}
+
+function sortearAmigo(){
+    let nomeSorteado = "";
+    let indice = Math.floor(Math.random()*nomesAmigos.length);
+    nomeSorteado = nomesAmigos[indice];
+    
+    resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+    let li = document.createElement("li");
+    li.textContent = `O amigo secreto sorteado é: ${nomeSorteado}`;
+    resultado.appendChild(li);
 }
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Amigo Secreto
+
+## 📌 Sobre o Projeto
+
+Este é um aplicativo web simples, proposto como desafio de um curso de Programação feito na Alura em parceria com a Oracle Next Education. 
+
+O sistema consiste em realizar sorteios de Amigo Secreto de forma prática e divertida. Com ele, os usuários adicionam nomes e pode sortear aleatoriamente um deles.
+
+## 🛠 Tecnologias Utilizadas
+
+O projeto foi desenvolvido utilizando as seguintes tecnologias:
+
+- **HTML5**: Para a estruturação da página.
+- **CSS3**: Para estilização e responsividade.
+- **JavaScript (ES6)**: Para a lógica de funcionamento, incluindo inclusão dos nomes e o sorteio aleatório nome para o amigo secreto.
+
+## 📋 Funcionalidades
+
+- Adicionar nomes à lista de participantes.
+- Sortear um nome aleatório da lista.
+- Exibir o resultado do sorteio de forma dinâmica na tela.
